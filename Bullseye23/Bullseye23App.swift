52bf7e9743801c279974c491af8f0d4304f32c26
Javier Gómez <javiergomezdev@gmail.com>
2023-07-14 12:01:52 -0500

    Initial Commit

---
+++ Bullseye23/Bullseye23App.swift
@@ -0,0 +1,17 @@
+//
+//  Bullseye23App.swift
+//  Bullseye23
+//
+//  Created by Javier Gomez on 14/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Bullseye23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
